c4013a8d4d0ecf5f5574a9575c1b909d08d6d984
Peter Jang <peter.x.jang@gmail.com>
2018-11-08 20:41:59 -0600

    Add more examples

--- practice.js
+++ practice.js
@@ -222,3 +222,78 @@ console.log(boat);
 console.log(boat.name);
 boat.color = "red";
 console.log(boat.color);
+
+// Write a function that accepts an array of strings and returns a new array containing every other string from the original array. For example, if the input is ["a", "b", "c", "d", "e", "f"], the output should be ["a", "c", "e"].
+function selectEvenItems(strings) {
+  var result = [];
+  var index = 0;
+  strings.forEach(function(string) {
+    if (index % 2 === 0) {
+      result.push(string);
+    }
+    index++;
+  });
+  return result;
+}
+console.log(selectEvenItems(["a", "b", "c", "d", "e", "f"]));
+
+// ------- or --------
+
+function selectEvenItemsV2(strings) {
+  var result = [];
+  for (var index = 0; index < strings.length - 1; index++) {
+    if (index % 2 === 0) {
+      result.push(strings[index]);
+    }
+  }
+  return result;
+}
+console.log(selectEvenItemsV2(["a", "b", "c", "d", "e", "f"]));
+
+// Write a method that accepts one argument - an array of numbers. The method should return the greatest number. For example, if the input is [5, 4, 8, 1, 2], the output should be 8.
+function max(numbers) {
+  var current_max = numbers[0];
+  numbers.forEach(function(number) {
+    if (number > current_max) {
+      current_max = number;
+    }
+  });
+  return current_max;
+}
+console.log(max([5, 4, 8, 1, 2]));
+
+// Write a method that accepts a number and returns its factorial. For example, the factorial of 5 is 5 * 4 * 3 * 2 * 1 = 120.
+function factorial(number) {
+  var result = 1;
+  var current_number = number;
+  for (var i = 0; i < number; i++) {
+    result = result * current_number;
+    current_number--;
+  }
+  return result;
+}
+console.log(factorial(5));
+
+// Write a method that accepts one argument - an array of numbers that are in ascending order. The method that returns a new array with the same values in descending order. However, do not use the "reverse" method built in to Ruby.
+function descending(numbers) {
+  var result = [];
+  var index = numbers.length - 1;
+  for (var i = 0; i < numbers.length; i++) {
+    result.push(numbers[index]);
+    index = index - 1;
+  }
+  return result;
+}
+console.log(descending([1, 3, 5, 7]));
+
+// Write a method that accepts two arrays of numbers, and returns an array of every sum of every combination of single numbers from first and second array. For example, if the method receives [1, 5, 10] and [100, 500, 1000], the method should return this array: [101, 501, 1001, 105, 505, 1005, 110, 510, 1010].
+function sumCombinations(numbers1, numbers2) {
+  var result = [];
+  numbers1.forEach(function(number1) {
+    numbers2.forEach(function(number2) {
+      result.push(number1 + number2);
+    });
+  });
+  return result;
+}
+console.log(sumCombinations([1, 5, 10], [100, 500, 1000]));
